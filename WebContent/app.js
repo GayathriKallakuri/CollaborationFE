dd5cd132d4173a8fdf7e830528d7ebc32f877fee
GayathriKallakuri <gayathri@192.168.2.12>
2017-01-09 20:38:29 +0530

    initial

---
+++ WebContent/app.js
@@ -0,0 +1,13 @@
+var app = angular.module("myApp",["ngRoute","ngCookies"]);
+app.config(function($routeProvider){
+	$routeProvider
+	.when("/",{
+		templateUrl : "c_home/home.html",
+		controller : "HomeController"
+	})
+	
+	.when("/register",{
+templateUrl : "c_user/register.html",
+controller : "UserController"
+})
+});
---
+++ WebContent/c_user/UserService.js
@@ -0,0 +1,106 @@
+'use strict';
+app
+		.factory(
+				'UserService',
+				[
+						'$http',
+						'$q',
+						'$rootscope',
+						function($http, $q, $rootscope) {
+							console.log("UserService...")
+							var BASE_URL = "http://localhost:8081/CollaborationBackEnd"
+							return {
+								fetchAllUsers : function() {
+									console.log("calling fetchAllUsers")
+									return $http.get(BASE_URL + "/users").then(
+											function(response) {
+												return response.data;
+											}, null);
+								},
+
+								accept : function(id) {
+									console.log("calling approve")
+									return $http
+											.get(BASE_URL + '/accept/' + id)
+											.then(
+													function(response) {
+														return response.data;
+													},
+													function(errResponse) {
+														console
+																.error('Error while accepting registration');
+														return $q
+																.reject(errResponse);
+													});
+								},
+
+								reject : function(id, reason) {
+									console.log("calling reject");
+									return $http
+											.get(
+													BASE_URL + '/reject/' + id
+															+ '/' + reason)
+											.then(
+													function(response) {
+														return response.data;
+													},
+													function(errResponse) {
+														console
+																.log("Error while rejecting registration");
+														return $q
+																.reject(errResponse);
+													});
+								},
+
+								createUser : function(user) {
+									console.log("calling create user");
+									return $http
+											.get(BASE_URL + '/user/', user)
+											.then(
+													function(response) {
+														return response.data;
+													},
+													function(errResponse) {
+														console
+																.error('Error while creating user');
+														return $q
+																.reject(errResponse);
+													});
+								},
+
+								updateUser : function(user, id) {
+									console.log("calling update user");
+									return $http
+											.put(BASE_URL + '/user/', user)
+											.then(
+													function(response) {
+														return response.data;
+													},
+													function(errResponse) {
+														console
+																.error('Error while updating user');
+														return $q
+																.reject(errResponse);
+													});
+								},
+
+								logout : function() {
+									console.log("logout calling...")
+									return $http.get(BASE_URL + '/user/logout')
+											.then(function(response) {
+												return response.data;
+											}, null);
+								},
+
+								authenticate : function(user) {
+									console
+											.log("calling the method authenticate user:"
+													+ user)
+									return $http.post(
+											BASE_URL + '/user/authenticate/',
+											user).then(function(response) {
+										return response.data;
+									}, null);
+								}
+							};
+						} ]);
